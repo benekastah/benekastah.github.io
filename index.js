90a8abb1a6815c7412656a19518816bc820ecfe1
Paul Harper <paul@chartio.com>
2016-01-04 11:01:55 -0800

    Such colors

--- index.js
+++ index.js
@@ -8,14 +8,37 @@ var answers = [
     'unwaveringly',
 ];
 
-var randomAnswer = function() {
-    return answers[Math.floor(Math.random() * answers.length)];
+var hexChars = [];
+for (var i = 0; i < 16; i++) {
+    hexChars.push(i.toString(16));
+}
+
+var randomChoice = function(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var randomAnswer = function () {
+    return randomChoice(answers);
+};
+
+var randomColor = function () {
+    var color = ['#']
+    for (var i = 0; i < 6; i++) {
+        color.push(randomChoice(hexChars));
+    }
+    return color.join('');
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     var answerEl = document.querySelector('#answer');
     setInterval(function () {
-        var answer = [randomAnswer(), randomAnswer()].join(' ') + '.';
+        document.body.style.backgroundColor = randomColor();
+        answerEl.style.color = randomColor();
+        var answers = [randomAnswer(), randomAnswer()];
+        if (answers[1] == answers[0]) {
+            answers[1] = randomAnswer();
+        }
+        var answer = answers.join(' ') + '.';
         answer = answer[0].toUpperCase() + answer.slice(1);
         answerEl.innerHTML = answer;
     }, 5000);
